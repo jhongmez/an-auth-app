e6f6e569abda94927a32dc28b99e2e5c87e399e5
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-11-30 08:25:07 -0500

    Add form reactive

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+	selector: 'app-login-page',
+	templateUrl: './login-page.component.html',
+	styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent {
+	
+	private formBuilder = inject( FormBuilder );
 
+	public myForm: FormGroup = this.formBuilder.group({
+		email: 		['', [ Validators.required, Validators.email ]],
+		password: 	['', [ Validators.required, Validators.minLength(6) ]],
+	})
+
+	login() {
+		console.log( this.myForm.value );
+	}
+	
 }
